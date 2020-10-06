f6a2b87be479eec66bf499eb089158ab92f65cd8
GitHub Action <action@github.com>
2020-10-06 03:44:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/carlalbrecht/intellijrustcodevision/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/carlalbrecht/intellijrustcodevision/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.carlalbrecht.intellijrustcodevision.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.carlalbrecht.intellijrustcodevision.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/carlalbrecht/intellijrustcodevision/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.carlalbrecht.intellijrustcodevision.services
+
+import com.github.carlalbrecht.intellijrustcodevision.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
