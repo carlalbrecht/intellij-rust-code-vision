e84e1247eda557a5e3ab6675bbddfd198dd2ece5
Carl Albrecht <invlpg@protonmail.com>
2020-10-09 11:45:45 +1100

    Added basic reference counting
    The reference count is not updated after the file has been loaded, and
    reference counts do not include parent references (e.g. trait method
    invocations through dynamic dispatch).

--- src/main/kotlin/com/github/carlalbrecht/intellijrustcodevision/listeners/FileEditorManagerListener.kt
+++ src/main/kotlin/com/github/carlalbrecht/intellijrustcodevision/listeners/FileEditorManagerListener.kt
@@ -8,6 +8,8 @@ import com.intellij.codeInsight.hints.presentation.AttributesTransformerPresenta
 import com.intellij.codeInsight.hints.presentation.InlayPresentation
 import com.intellij.codeInsight.hints.presentation.PresentationFactory
 import com.intellij.codeInsight.hints.presentation.RecursivelyUpdatingRootPresentation
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.impl.EditorImpl
 import com.intellij.openapi.fileEditor.FileEditor
@@ -19,6 +21,8 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Pair
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.search.searches.ReferencesSearch
+import com.intellij.util.concurrency.NonUrgentExecutor
 import org.rust.lang.core.psi.RsEnumItem
 import org.rust.lang.core.psi.RsFile
 import org.rust.lang.core.psi.RsFunction
@@ -27,6 +31,7 @@ import org.rust.lang.core.psi.RsStructItem
 import org.rust.lang.core.psi.RsTraitItem
 import org.rust.lang.core.psi.RsTypeAlias
 import java.awt.Color
+import java.util.concurrent.Callable
 
 internal class FileEditorManagerListener(val project: Project) : FileEditorManagerListener {
 
@@ -35,6 +40,8 @@ internal class FileEditorManagerListener(val project: Project) : FileEditorManag
         const val ICON_TEXT_SPACING = 4
         val FOREGROUND_COLOR = Color(0xA9, 0xA9, 0xA9)
     }
+
+    data class InlayInformation(val referenceCount: Int)
 
     data class Padding(val left: Int, val top: Int, val right: Int, val bottom: Int)
 
@@ -76,42 +83,87 @@ internal class FileEditorManagerListener(val project: Project) : FileEditorManag
                 override fun visitFunction(function: RsFunction) {
                     super.visitFunction(function)
 
-                    println("Found function: ${function.identifier.text}")
-                    createInlay(editor, presentationFactory, function.textOffset)
+                    ReadAction.nonBlocking(
+                        Callable {
+                            InlayInformation(
+                                ReferencesSearch.search(function).findAll().size
+                            )
+                        }
+                    ).inSmartMode(project).finishOnUiThread(ModalityState.current()) {
+                        println("Found function: ${function.identifier.text}")
+                        createInlay(editor, presentationFactory, function.textOffset, it)
+                    }.submit(NonUrgentExecutor.getInstance())
                 }
 
                 override fun visitStructItem(struct: RsStructItem) {
                     super.visitStructItem(struct)
 
-                    println("Found struct: ${struct.identifier?.text}")
-                    createInlay(editor, presentationFactory, struct.textOffset)
+                    ReadAction.nonBlocking(
+                        Callable {
+                            InlayInformation(
+                                ReferencesSearch.search(struct).findAll().size
+                            )
+                        }
+                    ).inSmartMode(project).finishOnUiThread(ModalityState.current()) {
+                        println("Found struct: ${struct.identifier?.text}")
+                        createInlay(editor, presentationFactory, struct.textOffset, it)
+                    }.submit(NonUrgentExecutor.getInstance())
                 }
 
                 override fun visitEnumItem(enum: RsEnumItem) {
                     super.visitEnumItem(enum)
 
-                    println("Found enum: ${enum.identifier?.text}")
-                    createInlay(editor, presentationFactory, enum.textOffset)
+                    ReadAction.nonBlocking(
+                        Callable {
+                            InlayInformation(
+                                ReferencesSearch.search(enum).findAll().size
+                            )
+                        }
+                    ).inSmartMode(project).finishOnUiThread(ModalityState.current()) {
+                        println("Found enum: ${enum.identifier?.text}")
+                        createInlay(editor, presentationFactory, enum.textOffset, it)
+                    }.submit(NonUrgentExecutor.getInstance())
                 }
 
                 override fun visitTraitItem(trait: RsTraitItem) {
                     super.visitTraitItem(trait)
 
-                    println("Found trait: ${trait.identifier?.text}")
-                    createInlay(editor, presentationFactory, trait.textOffset)
+                    ReadAction.nonBlocking(
+                        Callable {
+                            InlayInformation(
+                                ReferencesSearch.search(trait).findAll().size
+                            )
+                        }
+                    ).inSmartMode(project).finishOnUiThread(ModalityState.current()) {
+                        println("Found trait: ${trait.identifier?.text}")
+                        createInlay(editor, presentationFactory, trait.textOffset, it)
+                    }.submit(NonUrgentExecutor.getInstance())
                 }
 
                 override fun visitTypeAlias(typeAlias: RsTypeAlias) {
                     super.visitTypeAlias(typeAlias)
 
-                    println("Found type alias: ${typeAlias.identifier.text}")
-                    createInlay(editor, presentationFactory, typeAlias.textOffset)
+                    ReadAction.nonBlocking(
+                        Callable {
+                            InlayInformation(
+                                ReferencesSearch.search(typeAlias).findAll().size
+                            )
+                        }
+                    ).inSmartMode(project).finishOnUiThread(ModalityState.current()) {
+                        println("Found type alias: ${typeAlias.identifier.text}")
+                        createInlay(editor, presentationFactory, typeAlias.textOffset, it)
+                    }.submit(NonUrgentExecutor.getInstance())
                 }
             }
         )
     }
 
-    private fun createInlay(editor: Editor, presentationFactory: PresentationFactory, offset: Int) {
+    private fun createInlay(
+        editor: Editor,
+        presentationFactory: PresentationFactory,
+        offset: Int,
+        information: InlayInformation
+    ) {
         editor.inlayModel.addBlockElement(
             offset,
             false,
@@ -132,7 +184,8 @@ internal class FileEditorManagerListener(val project: Project) : FileEditorManag
                                 "This is a test tooltip :^)",
                                 inset(
                                     presentationFactory,
-                                    presentationFactory.smallText("42069 usages"),
+                                    // TODO pluralisation
+                                    presentationFactory.smallText("${information.referenceCount} usages"),
                                     Padding(ICON_TEXT_SPACING, 2, 0, 0)
                                 )
                             )
